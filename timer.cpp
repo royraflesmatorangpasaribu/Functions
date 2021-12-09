d7d72bf59b0ab21bd17fba8431b6cfa485a6352f
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2021-12-09 21:16:46 +0700

    Program C++

--- timer.cpp
+++ timer.cpp
@@ -11,11 +11,11 @@ void waktu(){
 	for(int i=5; i>=0 ; i--){
 		cout << "waktu : ";
 		cout << i;
-		Sleep(1000);   
+		Sleep(1000);   //jeda 1 detik
 		system("cls");	//menghapus layar
 	}
 }
-
+//minimal terdapat 1 fungsi 
 int main(){
 	//memanggil prosedur fungsi waktu
 	waktu();
