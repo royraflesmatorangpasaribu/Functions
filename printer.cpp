1313527b53aa3a94941a38c41dc8fbc7ee7cc192
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2022-01-04 21:46:05 +0700

    Update printer.cpp

--- printer.cpp
+++ printer.cpp
@@ -1,6 +1,10 @@
 #include<iostream>
 using namespace std;
 
+//program dibuat oleh roy rafles matorang pasaribu
+//pada 04 Januari 2021
+
+//prosedur fungsi
 void printChain(int n){
 	for(int i=0; i<n; i++){
 		cout<<i<<" ";
