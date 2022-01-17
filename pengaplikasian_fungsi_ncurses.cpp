f545a7929b62fc46abe254b23ab81b23f2f6d23e
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2022-01-17 16:35:47 +0700

    Add files via upload

---
+++ pengaplikasian_fungsi_ncurses.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include <ncurses/ncurses.h>
+#include<windows.h>
+using namespace std;
+int noPIN;
+string name;
+//string PIN[20] = {"123456"};
+
+void input(){
+	cout<<"Input Your Name : ";
+	cin>>name;
+	cout<<"Input Your PIN  : ";
+	cin>>noPIN;
+}
+void output(){
+	cout<<"Hello "<<name;
+}
+
+void loading(){
+	initscr();
+	mvprintw(10, 40, "Masuk Dalam Program");
+	mvprintw(11, 40, "Loading");
+	for(int baris=48; baris<=57; baris++){
+	mvprintw(11, baris, ".");
+	refresh();
+	Sleep(100);
+	}
+	
+	refresh();
+	getch();
+
+}
+int main(){
+	input();
+	system("CLS");
+	loading();
+	output();
+}
